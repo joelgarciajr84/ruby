9eba98e607c1b6e9d18a536b8648029bb32d7cf5
Joel Garcia jr <joel.garciajr84@gmail.com>
2016-03-21 15:30:53 -0300

    initial commit

---
+++ basics/types.rb
@@ -0,0 +1,26 @@
+#string
+name = "Dilma Roussef"
+puts name.class
+
+#fixnum
+age = 68
+puts age.class
+
+#float
+n = 6564646.454
+puts n.class
+
+#nil
+puts nil.class
+
+#hash
+cargos = {presidente:"Lula", cargo:"Ministra Chefe Casa Civil"}
+puts cargos.class
+
+#symbol
+puts :presidentedobrasil.class
+
+#array
+arr = []
+puts arr.class
+
