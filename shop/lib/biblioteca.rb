5010bff6523806a31dfc347b53dfe1c588d90179
Joel Garcia jr <joel.garciajr84@gmail.com>
2016-03-23 14:08:03 -0300

    Maps, selects yields

--- shop/lib/biblioteca.rb
+++ shop/lib/biblioteca.rb
@@ -3,19 +3,25 @@ class Biblioteca
 
     def initialize
         @livros = {} #Hash
+        @banco_de_arquivos = BancoDeArquivos.new
     end
 
     def adiciona(livro)
-        @livros[livro.categoria] ||= []
-        @livros[livro.categoria] << livro
+        salva livro do
+            livros << livro
+        end
     end
     def livros
         @livros.values.flatten
     end
 
     def livros_por_categoria(categoria)
-        @livros[categoria].each do |livro|
-            yield livro if block_given?
-        end
+        @livros.select { |livro| livro.categoria == categoria }
+    end
+
+    private
+    def salva(livro)
+        @banco_de_arquivos.salva livro
+        yield
     end
 end
--- shop/lib/shop.rb
+++ shop/lib/shop.rb
@@ -1,3 +1,4 @@
 require File.expand_path('shop/lib/livro.rb')
 require File.expand_path('shop/lib/biblioteca.rb')
 require File.expand_path('shop/lib/relatorio.rb')
+require File.expand_path('shop/lib/banco_de_arquivos.rb')
